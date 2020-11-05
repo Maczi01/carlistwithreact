0d7d0590db48b78867c047b7c54fd5d60f3a7c65
Maczi01 <mateusz.w.twardy@gmail.com>
2020-11-05 22:52:50 +0100

    add list and iteration

--- src/main/java/mati/pl/demo/controller/CarController.java
+++ src/main/java/mati/pl/demo/controller/CarController.java
@@ -4,14 +4,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class CarController {
 
 
     @GetMapping
     public String get(Model model){
-        Car car = new Car("Fiat", "126p");
-        model.addAttribute("car", car);
-        return "hello";
+        Car car1 = new Car("Fiat", "126p");
+        Car car2 = new Car("BMW", "3");
+        Car car3 = new Car("Fiat", "Stilo");
+        Car car4 = new Car("Fiat", "125p");
+        Car car5 = new Car("Fiat", "500");
+
+        List<Car> carList = Arrays.asList(car1,car2,car3, car3, car4, car5);
+        model.addAttribute("carList", carList);
+        return "car";
     }
 }
