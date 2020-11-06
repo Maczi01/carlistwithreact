fd8c01ffab81304fce5296c955d8e437ec5b0fa5
Maczi01 <mateusz.w.twardy@gmail.com>
2020-11-06 23:44:30 +0100

    add first version form

--- src/main/java/mati/pl/demo/controller/CarController.java
+++ src/main/java/mati/pl/demo/controller/CarController.java
@@ -3,6 +3,8 @@ package mati.pl.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Arrays;
 import java.util.List;
@@ -10,17 +12,27 @@ import java.util.List;
 @Controller
 public class CarController {
 
+    List<Car> carList;
 
-    @GetMapping
-    public String get(Model model){
+    public CarController() {
         Car car1 = new Car("Fiat", "126p");
         Car car2 = new Car("BMW", "3");
         Car car3 = new Car("Fiat", "Stilo");
         Car car4 = new Car("Fiat", "125p");
         Car car5 = new Car("Fiat", "500");
 
-        List<Car> carList = Arrays.asList(car1,car2,car3, car3, car4, car5);
+        carList = Arrays.asList(car1, car2, car3, car3, car4, car5);
+    }
+
+    @GetMapping
+    public String get(Model model) {
         model.addAttribute("carList", carList);
+        return "car";
+    }
+
+    @PostMapping
+    public String addCar(@ModelAttribute Car car){
+        carList.add(car);
         return "car";
     }
 }
